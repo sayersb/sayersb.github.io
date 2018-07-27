8b6b76b8c4aaacde600722955785377bb1d953c1
sayersb <bradleysayers1@gmail.com>
2018-07-27 17:03:39 +0100

    backgrounds and scroll functions added and working

--- js/app.js
+++ js/app.js
@@ -1,1 +1,19 @@
 console.log('js loaded');
+
+//  AOS.init({
+//   duration: 1200,
+// });
+
+$(document).ready(function () {
+
+  $(this).scrollTop(0);
+
+
+  $(function() {
+    $('a').on('click', function(e) {
+      e.preventDefault();
+      $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top}, 500, 'linear');
+    });
+  });
+
+});
